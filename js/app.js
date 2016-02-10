6b1a2c6972387d14d10cde18a6b19199a8143d28
Tim Reed <tim@alloftheabove.co.uk>
2016-02-10 16:00:26 +0000

    change to uuid

--- js/app.js
+++ js/app.js
@@ -7,7 +7,12 @@ function getBluetoothApi() {
 }
 
 function scanForDevices() {
-    navigator.bluetooth.requestDevice({ filters: [{ services: ['device_information'] }] })
+    navigator.bluetooth.requestDevice({ 
+        // filters: [{ services: ['device_information'] }]         
+        filters: [{
+            services: [0x1234, 0x12345678, 'b9407f30-f5f8-aff9-25556b57fe6d']
+        }] 
+    })
     .then(device => {
         console.log('device', device);
     })
